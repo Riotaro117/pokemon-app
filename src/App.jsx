30f9cdba40a540b459b7d4fa3fea47aa00055383
riotaro <rugby117117@gmail.com>
2025-12-17 06:13:13 +0900

    ポケモン１匹ずつのデータを取得

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,43 @@
 import { useEffect, useState } from 'react';
 import './App.css';
-import { getAllPokemon } from './utils/pokemon';
+import { getAllPokemon, getPokemon } from './utils/pokemon';
 
 function App() {
   // ENDPOINTの設定
   const initialURL = 'https://pokeapi.co/api/v2/pokemon';
   const [loading, setLoading] = useState(true);
+  const [pokemonData, setPokemonData] = useState([]);
+
+  // ポケモン１匹ずつのデータを取得してくる
+  const loadPokemon = async (data) => {
+    // Promise.allなのはポケモン１匹ずつ全て取得するまで待つということ
+    let _pokemonData = await Promise.all(
+      data.map((pokemon) => {
+        let pokemonRecord = getPokemon(pokemon.url);
+        return pokemonRecord;
+      })
+    );
+    setPokemonData(_pokemonData);
+  };
 
   //非同期の処理はuseEffect
   useEffect(() => {
     const fetchPokemonData = async () => {
       // 全てのポケモンデータを取得
       let res = await getAllPokemon(initialURL);
+      // console.log(res)
+
       // 各ポケモンの詳細なデータを取得
-      loadPokemon();
+      loadPokemon(res.results);
+      // console.log(res.results)
+
       // ちゃんとロードが完了したため、ロード画面は必要ないfalse
       setLoading(false);
     };
     fetchPokemonData();
   }, []);
+
+  console.log(pokemonData);
 
   return (
     <div className="App">
--- src/utils/pokemon.js
+++ src/utils/pokemon.js
@@ -1,7 +1,19 @@
 export const getAllPokemon = (url) => {
-  return new Promise((resolve, reject) => {
+  return new Promise((resolve) => {
     fetch(url)
       .then((res) => res.json())
       .then((data) => resolve(data));
+  });
+};
+
+// １匹ずつのポケモンデータを取得
+export const getPokemon = (url) => {
+  return new Promise((resolve) => {
+    fetch(url)
+      .then((res) => res.json())
+      .then((data) => {
+        resolve(data);
+        // console.log(data);
+      });
   });
 };
